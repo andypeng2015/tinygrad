beb2cb7e9436918e29bf45accb4b8a3242df0559
qazal <qazal.software@gmail.com>
2025-01-23 19:08:47 +0200

    merge the views [pr]

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -1272,7 +1272,7 @@ ConstLike = Union[ConstType, Variable, tuple[ConstType, ...]]
 # *** uop swizzling ***
 
 merge_views = PatternMatcher([
-  (UPat(Ops.VIEW, name="s0").view(name="s1"), lambda s0,s1: s0.replace(arg=s0.st+s1.st)),
+  (UPat(Ops.VIEW, name="vm2", src=(UPat(Ops.VIEW, name="vm1"),)), lambda vm1,vm2: vm1.replace(arg=vm1.st+vm2.st)),
   (UPat(Ops.VIEW, name="mv", src=(UPat.var("x"),)), lambda mv,x: x if mv.st.contiguous and x.st is not None and x.shape == mv.shape else None),
 ])
 
