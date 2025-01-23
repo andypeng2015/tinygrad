78eb4ac71c7208193ec873d5fc54f59a15df64e0
qazal <qazal.software@gmail.com>
2025-01-23 18:21:49 +0200

    add Tensor UOp to ScheduleItem process replay [pr]

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # compare kernels created by HEAD against master
-from collections import defaultdict
 import os, multiprocessing, logging, pickle, sqlite3, difflib, functools, warnings
 from typing import Callable, cast
 from tinygrad.helpers import VERSION, Context, ContextVar, colored, db_connection, getenv, tqdm
-from tinygrad.engine.schedule import ScheduleContext, schedule_uop
+from tinygrad.engine.schedule import create_schedule_with_vars
 from tinygrad.codegen.kernel import Kernel, Opt
 from tinygrad.renderer import Renderer
 from tinygrad.ops import UOp
@@ -30,9 +29,9 @@ class ProcessReplayWarning(Warning): pass
 
 # *** recreators
 
-def recreate_sched(ast:UOp) -> UOp:
-  # NOTE: process replay isn't meant to actually schedule anything
-  return schedule_uop(ast, ScheduleContext(tensor_uops=defaultdict(list))).ast
+def recreate_sched(big_sink:UOp) -> list[UOp]:
+  schedule, _, __ = create_schedule_with_vars(big_sink)
+  return [x.ast for x in schedule]
 def recreate_kernel(ast:UOp, opts:Renderer, applied_opts:list[Opt], name:str) -> str:
   k = Kernel(ast, opts=opts)
   for opt in applied_opts: k.apply_opt(opt)
@@ -68,7 +67,7 @@ def diff(offset:int, name:str, fxn:Callable) -> tuple[int, int]|bool:
       for x in args[:-1]: logging.info(x)
       continue
     # diff kernels
-    try: assert args[-1] == good
+    try: assert str(args[-1]) == str(good)
     except AssertionError:
       changed += 1
       logging.info("PROCESS REPLAY DETECTED CHANGE")
